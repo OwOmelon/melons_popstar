2a09b70ff5124bce6924e2d32b9f378a732019ce
melon <cccaaarrrlll99@gmail.com>
2023-12-02 20:29:26 +0800

    new project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p class="rounded bg-red-400 p-2 font-bold text-white">
+    new fuckin project babyyyy
+  </p>
+  <p>um</p>
+</template>
+
+<style lang="postcss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600;800&display=swap");
+
+*,
+*::before,
+*::after {
+  @apply m-0 box-border p-0;
+}
+
+body {
+   @apply bg-amber-100 font-["Poppins",_sans-serrif]
+}
+
+#app {
+  @apply flex min-h-screen flex-col items-center justify-center;
+}
+</style>
