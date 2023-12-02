7a24ef71dc6e8df21b1e4e9b46d78bc974b87901
melon <cccaaarrrlll99@gmail.com>
2023-12-02 21:15:30 +0800

    tile component

---
+++ src/components/Tile.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { Tile } from "@/stores/board";
+
+const props = defineProps<Tile>();
+</script>
+
+<template>
+	<div
+		:style="{ backgroundColor: color }"
+		class="grid aspect-square w-10 place-items-center rounded text-xs"
+	>
+		<p>{{ pos.x }} | {{ pos.y }}</p>
+	</div>
+</template>
