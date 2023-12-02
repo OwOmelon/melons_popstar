8ca53273eceb3588e596349e66db6d861fd6770a
melon <cccaaarrrlll99@gmail.com>
2023-12-02 20:54:33 +0800

    Board component

---
+++ src/components/Board.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useBoardStore } from "@/stores/board";
+
+const board = useBoardStore();
+</script>
+
+<template>
+	<div class="flex gap-2">
+		<div class="flex flex-col gap-2" v-for="column in board.board">
+			<div
+				v-for="tile in column"
+				:style="{ backgroundColor: tile.color }"
+				class="grid place-items-center aspect-square w-10 rounded text-xs"
+			>
+				<p>{{ tile.pos.x }} | {{ tile.pos.y }}</p>
+			</div>
+		</div>
+	</div>
+</template>
