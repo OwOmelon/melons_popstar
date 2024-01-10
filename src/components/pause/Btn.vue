b09b11431451236c0d3a1a8ae2ce4a0f5fb9168b
melon <cccaaarrrlll99@gmail.com>
2024-01-10 12:06:56 +0800

    pause button

---
+++ src/components/pause/Btn.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useGameStateStore } from "@/stores/game_state";
+import PauseIcon from "~icons/solar/pause-bold";
+
+const game_state = useGameStateStore();
+</script>
+
+<template>
+	<button type="button" @click="game_state.paused = !game_state.paused">
+		<PauseIcon />
+	</button>
+</template>
