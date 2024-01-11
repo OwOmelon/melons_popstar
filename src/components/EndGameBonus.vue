ac3ea18cac7593884c5b75e0ab4006a30572645f
melon <cccaaarrrlll99@gmail.com>
2024-01-11 10:10:13 +0800

    component rename: GetEndGameBonus -> EndGameBonus

---
+++ src/components/EndGameBonus.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+	bonus: number;
+}>()
+</script>
+
+<template>
+	<div class="flex justify-center">
+		<span class="text-4xl font-bold">{{ bonus }}</span>
+	</div>
+</template>
