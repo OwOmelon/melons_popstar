cd3f4c0c2f59d95417eadb641a9b751f6202138c
melon <cccaaarrrlll99@gmail.com>
2024-01-24 00:29:47 +0800

    add fancy spinning star as bg for pts text

--- src/eyecandy/clear-pts-anim.ts
+++ src/eyecandy/clear-pts-anim.ts
@@ -4,12 +4,22 @@ import type { TileID } from "../stores/board";
 
 export function clearPtsAnim(id: TileID, points: number): void {
 	const vfxBoard: HTMLElement = document.getElementById("vfx-board")!;
-	const pts: HTMLElement = createPtsEl();
+	const vfxBoardRect: DOMRect = vfxBoard.getBoundingClientRect();
+
+	const tile: HTMLElement = document.getElementById(id)!;
+	const tileRect: DOMRect = tile.getBoundingClientRect();
+
+	const fx = createContainer(tileRect, vfxBoardRect);
+	const star = createStarSvg(tileRect);
+	const pts = createPtsEl(points);
 	const animDur: number = 800;
 
-	vfxBoard.appendChild(pts);
+	fx.appendChild(star);
+	fx.appendChild(pts);
 
-	pts.animate(
+	vfxBoard.appendChild(fx);
+
+	fx.animate(
 		[
 			{
 				opacity: "1",
@@ -17,49 +27,84 @@ export function clearPtsAnim(id: TileID, points: number): void {
 			},
 			{
 				opacity: "0",
-				transform: "translateY(-20px)",
+				transform: "translateY(-50px)",
 			},
 		],
-		{ duration: animDur, fill: "forwards", easing: 'ease' },
+		{ duration: animDur, fill: "forwards", easing: "ease" },
+	);
+
+	star.animate(
+		[
+			{
+				transform: "scale(1) rotate(0)",
+			},
+			{
+				transform: "scale(2) rotate(180deg)",
+			},
+		],
+		{ duration: animDur, fill: "forwards", easing: "ease" },
 	);
 
 	setTimeout(() => {
-		pts.remove();
+		fx.remove();
 	}, animDur);
+}
 
-	function createPtsEl(): HTMLElement {
-		const vfxBoardRect: DOMRect = vfxBoard.getBoundingClientRect();
-		const tileRect: DOMRect = document
-			.getElementById(id)!
-			.getBoundingClientRect();
+function createContainer(
+	tileRect: DOMRect,
+	vfxBoardRect: DOMRect,
+): HTMLElement {
+	const div: HTMLDivElement = document.createElement("div");
 
-		const div: HTMLDivElement = document.createElement("div");
-		const text: HTMLParagraphElement = document.createElement("p");
+	div.style.left = `${tileRect.left - vfxBoardRect.left}px`;
+	div.style.top = `${tileRect.top - vfxBoardRect.top}px`;
 
-		div.style.left = `${tileRect.left - vfxBoardRect.left}px`;
-		div.style.top = `${tileRect.top - vfxBoardRect.top}px`;
+	div.style.width = `${tileRect.width}px`;
+	div.style.height = `${tileRect.height}px`;
 
-		div.style.width = `${tileRect.width}px`;
-		div.style.height = `${tileRect.height}px`;
+	div.classList.add(
+		"pointer-events-none",
+		"absolute",
+		"grid",
+		"place-items-center",
+	);
 
-		div.classList.add(
-			"pointer-events-none",
-			"absolute",
-			"text-xl",
-			"font-bold",
-		);
+	return div;
+}
 
-		text.innerText = points.toString();
-		text.classList.add(
-			"absolute",
-			"left-1/2",
-			"top-1/2",
-			"-translate-x-1/2",
-			"-translate-y-1/2",
-		);
+function createPtsEl(points: number): HTMLElement {
+	const text: HTMLParagraphElement = document.createElement("p");
 
-		div.appendChild(text);
+	text.innerText = points.toString();
+	text.classList.add("absolute", "text-xl", "font-bold");
 
-		return div;
-	}
+	return text;
+}
+
+function createStarSvg(tileRect: DOMRect): SVGSVGElement {
+	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+	svg.setAttribute("viewBox", "0 0 24 24");
+	svg.setAttribute("width", `${tileRect.width}`);
+	svg.setAttribute("height", `${tileRect.width}`);
+	svg.setAttributeNS(
+		"http://www.w3.org/2000/xmlns/",
+		"xmlns:xlink",
+		"http://www.w3.org/1999/xlink",
+	);
+
+	svg.style.position = "absolute";
+	svg.style.color = "#fff";
+	svg.style.filter = "drop-shadow(0 0 1px rgba(0, 0, 0, 0.75))";
+
+	path.setAttribute("fill", "currentColor");
+	path.setAttribute(
+		"d",
+		"m8.125 7.092l2.608-3.47q.238-.321.566-.472Q11.627 3 12 3q.373 0 .701.15q.328.15.566.471l2.608 3.471l4.02 1.368q.534.18.822.606q.289.425.289.939q0 .237-.07.471q-.07.234-.228.449l-2.635 3.573l.1 3.83q.025.706-.466 1.189q-.492.483-1.144.483l-.453-.056L12 18.733l-4.11 1.211q-.125.05-.24.053q-.117.003-.213.003q-.666 0-1.15-.483q-.485-.483-.46-1.188l.1-3.856l-2.629-3.548q-.159-.217-.228-.453Q3 10.236 3 10q0-.506.297-.942q.296-.435.828-.618z",
+	);
+
+	svg.appendChild(path);
+
+	return svg;
 }
