1a7b6e745b3984365acb34a42876c2e676d9f108
melon <cccaaarrrlll99@gmail.com>
2025-01-15 17:51:10 +0800

    get data directly from store instead of through props

--- src/gamestate/components/EndGameBonus.vue
+++ src/gamestate/components/EndGameBonus.vue
@@ -1,26 +1,25 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
+import { storeToRefs } from "pinia";
+import { useGameStateStore } from "../stores/gamestate";
 
-const props = defineProps<{
-	bonus: number | null;
-}>();
+const { endGameBonus } = storeToRefs(useGameStateStore());
 
+// !!! IS THERE EVEN A POINT TO bonusDisplay ???
+//  !!! ADD V-IF ?
 const bonusDisplay = ref<number>(2000);
 
-watch(
-	() => props.bonus,
-	(val) => {
-		if (val === null) return
+watch(endGameBonus, (val) => {
+	if (val === null) return;
 
-		bonusDisplay.value = val
-	},
-);
+	bonusDisplay.value = val;
+});
 </script>
 
 <template>
 	<div
 		:class="[
-				{'opacity-0': bonus === null},
+			{ 'opacity-0': endGameBonus === null },
 			'shadow-subtle mx-auto my-4 my-auto flex translate-y-3 items-center justify-center gap-4 rounded border-2 border-white bg-lime-300 px-4 py-2 text-white transition-opacity duration-1000',
 		]"
 	>
