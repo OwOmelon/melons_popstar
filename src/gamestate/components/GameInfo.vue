9362333eeeddebd580df607844deb59e50ab2271
melon <cccaaarrrlll99@gmail.com>
2025-01-15 17:36:00 +0800

    get data directly from store instead of through props what the hell was i thinking ?

--- src/gamestate/components/GameInfo.vue
+++ src/gamestate/components/GameInfo.vue
@@ -1,25 +1,20 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
+import { storeToRefs } from "pinia";
+import { useGameStateStore } from "../stores/gamestate";
 
-const props = defineProps<{
-	points: number;
-	goal: number;
-	stage: number;
-	stagePass: boolean;
-}>();
+const { points, goal, stage, stagePass } = storeToRefs(useGameStateStore());
 
-const key = ref<number>(0);
+// !!! IS THERE REALLY A POINT TO pointsGainedEl_Key ???
+const pointsGainedEl_Key = ref<number>(0);
 const pointsGained = ref<number>(0);
 
-watch(
-	() => props.points,
-	(newPts, oldPts) => {
-		if (newPts === 0) return;
+watch(points, (newPts, oldPts) => {
+	if (newPts === 0) return;
 
-		key.value++;
-		pointsGained.value = newPts - oldPts;
-	},
-);
+	pointsGainedEl_Key.value++;
+	pointsGained.value = newPts - oldPts;
+});
 </script>
 
 <template>
@@ -29,7 +24,7 @@ watch(
 		<span
 			:class="[
 				{
-					'shadow-subtle shine !border-white bg-amber-400 text-white':
+					'shadow-subtle glint !border-white bg-amber-400 text-white':
 						stagePass,
 				},
 				'absolute left-1/2 top-1/2 mx-auto w-fit -translate-x-1/2 -translate-y-1/2 rounded border-2 border-transparent px-4 py-2 text-center text-[2.25em] transition-colors',
@@ -52,7 +47,7 @@ watch(
 		<div
 			class="absolute left-1/2 top-1/2 grid -translate-x-1/2 -translate-y-[calc(50%_-_2rem)] place-items-center"
 		>
-			<span :key="key" class="jump absolute whitespace-nowrap text-xl">
+			<span :key="pointsGainedEl_Key" class="jump absolute whitespace-nowrap text-xl">
 				+ {{ pointsGained }}
 			</span>
 		</div>
@@ -80,7 +75,7 @@ watch(
 	}
 }
 
-.shine {
+.glint {
 	background-image: linear-gradient(
 		to right,
 		rgba(255, 255, 255, 0) 0%,
@@ -91,10 +86,10 @@ watch(
 	background-size: 200% 100%;
 	background-repeat: no-repeat;
 
-	animation: shine 5s linear infinite;
+	animation: glint 5s linear infinite;
 }
 
-@keyframes shine {
+@keyframes glint {
 	0% {
 		background-position: 50% 0;
 	}
