712ab337036b9b599b5fb87ca79ed8fee0eb1bb1
melon <cccaaarrrlll99@gmail.com>
2025-01-23 22:00:15 +0800

    rework board clear bonus hide/show logic

--- src/gamestate/components/EndGameBonus.vue
+++ src/gamestate/components/EndGameBonus.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
+
 const { boardClearBonus } = defineProps<{
 	boardClearBonus: number;
 }>();
+
+const display = ref(0);
+
+watch(
+	() => boardClearBonus,
+	(b) => {
+		if (b === -1) return;
+
+		display.value = b;
+	},
+);
 </script>
 
 <template>
-	<Transition name="fade">
-		<div
-			v-if="boardClearBonus !== -1"
-			class="shadow-subtle mx-auto my-4 my-auto flex translate-y-3 items-center justify-center gap-4 rounded border-2 border-white bg-lime-300 px-4 py-2 text-white"
-		>
-			<span>bonus: </span>
-			<span class="text-4xl font-bold">{{ boardClearBonus }}</span>
+	<div
+		:class="[
+			{
+				'pointer-events-none opacity-0 duration-[1.5s]': boardClearBonus === -1,
+			},
+			'shadow-subtle mx-auto my-4 my-auto flex items-center justify-center gap-4 rounded border-2 border-white bg-lime-300 px-4 py-2 text-white transition-opacity duration-500',
+		]"
+	>
+		bonus:
+		<div class="text-4xl font-bold">
+			{{ display }}
 		</div>
-	</Transition>
+	</div>
 </template>
