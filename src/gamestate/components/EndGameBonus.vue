1ace346e7b0a248eda747d90e87461b74cfb692f
melon <cccaaarrrlll99@gmail.com>
2025-01-23 17:18:16 +0800

    move hide/show logic for <EndGameBonus> from outside to inside
    just to make <App> look cleaner

--- src/gamestate/components/EndGameBonus.vue
+++ src/gamestate/components/EndGameBonus.vue
@@ -5,10 +5,13 @@ const { boardClearBonus } = defineProps<{
 </script>
 
 <template>
-	<div
-		class="shadow-subtle mx-auto my-4 my-auto flex translate-y-3 items-center justify-center gap-4 rounded border-2 border-white bg-lime-300 px-4 py-2 text-white"
-	>
-		<span>bonus: </span>
-		<span class="text-4xl font-bold">{{ boardClearBonus }}</span>
-	</div>
+	<Transition name="fade">
+		<div
+			v-if="boardClearBonus !== -1"
+			class="shadow-subtle mx-auto my-4 my-auto flex translate-y-3 items-center justify-center gap-4 rounded border-2 border-white bg-lime-300 px-4 py-2 text-white"
+		>
+			<span>bonus: </span>
+			<span class="text-4xl font-bold">{{ boardClearBonus }}</span>
+		</div>
+	</Transition>
 </template>
