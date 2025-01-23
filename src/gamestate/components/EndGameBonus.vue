e759ac0b7c93ec251866c4eaa8aaf7c586aebd80
melon <cccaaarrrlll99@gmail.com>
2025-01-23 16:25:22 +0800

    remove dynamic styles from <EndGameBonus>

--- src/gamestate/components/EndGameBonus.vue
+++ src/gamestate/components/EndGameBonus.vue
@@ -6,10 +6,7 @@ const { boardClearBonus } = defineProps<{
 
 <template>
 	<div
-		:class="[
-			{ 'pointer-events-none opacity-0': boardClearBonus === -1 },
-			'shadow-subtle mx-auto my-4 my-auto flex translate-y-3 items-center justify-center gap-4 rounded border-2 border-white bg-lime-300 px-4 py-2 text-white transition-opacity duration-1000',
-		]"
+		class="shadow-subtle mx-auto my-4 my-auto flex translate-y-3 items-center justify-center gap-4 rounded border-2 border-white bg-lime-300 px-4 py-2 text-white"
 	>
 		<span>bonus: </span>
 		<span class="text-4xl font-bold">{{ boardClearBonus }}</span>
