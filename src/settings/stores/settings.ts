338e27540ed9a67b79f23cc921a84d76516938ed
melon <cccaaarrrlll99@gmail.com>
2025-01-30 12:50:22 +0800

    add "full screen" toggle

--- src/settings/stores/settings.ts
+++ src/settings/stores/settings.ts
@@ -1,23 +1,56 @@
-import { ref } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { defineStore } from "pinia";
-import { useStorage } from "@vueuse/core";
+import { useStorage, useFullscreen } from "@vueuse/core";
 
-type SettingKey = "bgAnim" | "tileClearAnim" | "boardShake";
+type SettingKey = "bgAnim" | "tileClearAnim" | "boardShake" | "fullScreen";
+type Settings_Toggles = Record<
+	SettingKey,
+	{ displayName: string; toggled: boolean }
+>;
 
 export const useSettingsStore = defineStore("settings", () => {
 	const paused = ref(false);
 
-	const settings_Toggles = useStorage<
-		Record<SettingKey, { displayName: string; toggled: boolean }>
+	// ==========
+
+	const docEl = ref<HTMLElement | null>(null);
+
+	const { isFullscreen, toggle: toggleFullScreen } = useFullscreen(docEl);
+
+	const settings_Toggles_UserPreferences = useStorage<
+		Partial<Settings_Toggles>
 	>("settings_toggles", {
 		bgAnim: { displayName: "background animation", toggled: true },
 		tileClearAnim: { displayName: "tile clear animation", toggled: true },
 		boardShake: { displayName: "board shake", toggled: true },
 	});
 
+	const settings_Toggles = computed(() => {
+		return {
+			...settings_Toggles_UserPreferences.value,
+			fullScreen: { displayName: "full screen", toggled: isFullscreen.value },
+		} as Settings_Toggles;
+	});
+
 	function toggleSetting(key: SettingKey) {
-		settings_Toggles.value[key].toggled = !settings_Toggles.value[key].toggled;
+		if (key === "fullScreen") {
+			toggleFullScreen();
+
+			return;
+		}
+
+		const setting = settings_Toggles.value[key];
+
+		if (!setting) return;
+
+		setting.toggled = !setting.toggled;
 	}
+
+	onMounted(() => {
+		docEl.value = document.documentElement;
+	});
+
+	// ==========
 
 	const changingBoardSize = ref<boolean>(false);
 	const areaFontSize = ref(16);
